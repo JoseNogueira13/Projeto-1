a92430da85f99fe5728e13744cfc9619e54d4364
JoseNogueira13 <92999470+JoseNogueira13@users.noreply.github.com>
2022-05-25 12:52:53 +0100

    Add files via upload

---
+++ Classes/RecipeModel.js
@@ -0,0 +1,16 @@
+class recipe {
+    image = "";
+    title = "";
+    description = "";
+    recipeWritten = "";
+    recipeVideo = "";
+    type = "";
+    constructor(image, title, description, recipeWritten, recipeVideo, type){
+        this.image = image;
+        this.title = title;
+        this.description = description;
+        this.recipeWritten = recipeWritten;
+        this.recipeVideo = recipeVideo;
+        this.type = type;
+    }
+}
---
+++ Classes/UserModel.js
@@ -0,0 +1,16 @@
+class user {
+    username = "";
+    password = "";
+    email = "";
+    experience = "";
+    badges = "";
+    saved = "";
+    constructor(username, password, email, experience, badges, saved) {
+        this.username = username;
+        this.password = password;
+        this.email = email;
+        this.experience = experience;
+        this.badges = badges;
+        this.saved = saved;
+      }
+}
