3aad5c12d6975585541c55f95f036fa33ae4437e
JoseNogueira13 <92999470+JoseNogueira13@users.noreply.github.com>
2022-05-31 19:40:36 +0100

    Login / Register views

--- Projeto1_FoodEat/js/Views/NavbarView.js
+++ Projeto1_FoodEat/js/Views/NavbarView.js
@@ -23,14 +23,42 @@ function navbarView() {
     
     if (User.isLogged()) {
         result += `
-        <button class="btn btn-primary" type="button">
+        <button id="btnUser" class="btn btn-primary" type="button">
             <img src="${User.getUserLogged().avatar}">
         </button>`
     } else {
         result += `
-        <button class="btn btn-primary" type="button">LOGIN/REGISTER</button>`
+        <button id="btnUserLogin" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#mdlLogin">Login</button>
+        <button id="btnUserRegister" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#mdlRegister">Register</button>`
     }
+    // colocar o conteudo na navbar dependendo do estado de login do utilizador
 document.querySelector("nav").innerHTML = result;   
+
+// BOTÃO DE LOGIN
+document.querySelector("#frmLogin").addEventListener("submit",(event) => {
+    event.preventDefault();
+    try {
+        User.login(
+            document.getElementById("txtUsername").value,
+            document.getElementById("txtPassword").value,
+        );
+        signupMessage("msgLogin", "User logged in with sucess!", "sucess");
+
+        setTimeout(() => {
+            location.reload();
+        }, 1000);
+    }catch (error)  {
+        signupMessage("msgLogin", error.message, "danger");
+    }
+})
+}
+
+function signupMessage(modal, message, type) {
+    const divMessage = document.getElementById(modal);
+    divMessage.innerHTML = `<div class = "alert alert-${type}" role = "alert"> ${message}</div>`;
+    setTimeout(() => {
+        divMessage.innerHTML = ""
+    }, 1000);
 }
 
 navbarView();
